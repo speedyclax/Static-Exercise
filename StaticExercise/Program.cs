38fb862c4a4e2d041fccacf1f22512490cbff863
Jon Claxton <speedyclax@gmail.com>
2023-06-22 22:45:04 -0500

    exercise1 completed degree converter

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var Celsius = TempConverter.FahrenheitToCelsius(212);
+            Console.WriteLine($"{Celsius} degrees Celsius");
+
+            Console.WriteLine();
+
+            var Fahrenheit = TempConverter.CelsiusToFahrenheit(100);
+            Console.WriteLine($"{Fahrenheit} degrees Fahrenheit");
+        }
+    }
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius (double F)
+        {
+            return ((F - 32) * 5/9);
+        }   
+        public static double CelsiusToFahrenheit (double C)
+        {
+            return (C * 9/5 + 32);
         }
     }
 }
